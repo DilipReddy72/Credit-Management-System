64a9337d92d0af9443aadf77ad6c19905af49f96
Sai Charan 2806 <54449077+onlineNoob@users.noreply.github.com>
2020-09-23 18:11:13 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,13 @@
-<?php
+//Below php code establishes the connection between the file and the database.
 
+<?php
+	// Assigning the database values to the separate variables.
 	$username = "root";
 	$password = "";
 	$server = 'localhost';
 	$db = "credits";	
-
+	
+	//Creating a connection.
 	$con = mysqli_connect($server,$username,$password,$db);
 
 	if ($con) {
