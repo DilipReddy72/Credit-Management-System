d3a223ae421780bac60195836b0d69e0726ef02c
Sai Charan 2806 <54449077+onlineNoob@users.noreply.github.com>
2020-09-23 18:16:34 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -1,3 +1,4 @@
+//Below php code creates a connection to the file and database.
 <?php
 include 'connection.php';
 
@@ -5,17 +6,19 @@ include 'connection.php';
 <!DOCTYPE html>
 <html>
 <head>
+	//This head tag consists the title and the link to the css of this page.
 	<title>Transfer Credits</title>
 	<link rel="stylesheet" type="text/css" href="transfer1.css">
 	
 </head>
 <body>
 
+	//ALigning every item to the center.
 <center>
 	<h1>Transfer Credits</h1>
 
 
-
+	//Creating a form to transfer the credits.
 	<form action="" method="GET">
 		<div class="field"><input type="text" name="id" placeholder="ID:" required=""><br>
 		<input type="text" name="name"  placeholder="Name:" required=""><br>
@@ -25,13 +28,14 @@ include 'connection.php';
 		
 	</form>
 </center>
+	//Creating a link to go back to the users page.
 <div class="back"> <a href="Users.php">View Users</a></div>
 
 </body>
 </html>
 
 <?php
-
+//This php code updates the entered value from the existed value in the database.
 if(isset($_GET['submit'])) {
 	$id = $_GET['id'];
 	$credits = $_GET['credits'];
